3bfcd93693c67480b35343a5549fa0f0f6b2dc2b
Ahmad Ridzwan <ahmadridzwan91@gmail.com>
2024-10-07 20:39:05 +0400

    Implement sidebars and doughnut chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = { firstName: 'Ahmad Ridzwan' };
+  const loggedIn = { firstName: 'Ahmad Ridzwan', lastName: 'Zainuddin', email: 'ahmadridzwan91@gmail.com' };
 
   return (
     <section className='home'>
@@ -14,8 +16,21 @@ const Home = () => {
             user = { loggedIn?.firstName || 'Guest' }
             subtext = 'Access and manage your accounts and transactions efficiently.'
           />
+
+          <TotalBalanceBox 
+            accounts={ [] }
+            totalBanks = {1}
+            totalCurrentBalance = {1250.35}
+          />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user = {loggedIn}
+        transactions = { [] }
+        banks = { [ {}, {} ] }
+      />
     </section>
   )
 }
